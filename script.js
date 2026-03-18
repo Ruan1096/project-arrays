c2fe84101bef628114efb545beb673918798b437
Ruan1096 <ruanreis1996@gmail.com>
2026-03-18 11:56:18 -0300

    segunda alteração

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const menuOptions = [
-{ name: 'X-Salada', price: 30, vegan: false, src: './img/xsalada.jpeg' },
-{ name: 'X-Bacon', price: 34, vegan: false, src: './img/xbacon.png' },
-{ name: 'X-Bacon Egg', price: 39, vegan: false, src: './img/bacon-egg.png' },
-{ name: 'Monstruoso', price: 50, vegan: false, src: './img/monstruoso.png' },
-{ name: 'Big Vegano', price: 55, vegan: true, src: './img/xvegan.png' },
-{ name: 'X-Vegan', price: 45, vegan: true, src: './img/monstruoso-vegan.png' },
-]
+const list = document.querySelector('ul')
+const buttonshowAll = document.querySelector('.show-all')
+let myLi = ''
+
+function showAll() {
+    menuOptions.forEach((product) => {
+    myLi = myLi + `
+            <li>
+                <img src="${product.src}" alt="${product.name}">
+                <p>${product.name}</p>
+                <p class="inten-price">${product.price}</p>
+            </li> 
+            `       
+})
+
+list.innerHTML = myLi
+}
+
+buttonshowAll.addEventListener('click', showAll)
