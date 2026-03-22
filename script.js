8b258cfc763ae6a5dcb3dfa06119b9f7696ad135
Ruan1096 <ruanreis1996@gmail.com>
2026-03-22 17:02:35 -0300

    alteração do reduce

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const list = document.querySelector('ul')
 const buttonshowAll = document.querySelector('.show-all')
 const buttonMapAll = document.querySelector('.map-all')
+const sumAll = document.querySelector('.sum-all')   
 
 
 function showAll( productsArray ) {
@@ -8,7 +9,8 @@ function showAll( productsArray ) {
     let myLi = ''
 
     productsArray.forEach((product) => {
-    myLi = myLi + `
+    myLi = myLi + 
+            `
             <li>
                 <img src="${product.src}" alt="${product.name}">
                 <p>${product.name}</p>
@@ -30,7 +32,22 @@ function mapAllItems() {
     }))
     showAll(newPrice)
     console.log(newPrice)
-}   
+}
+
+    function sumAllItems() {
+        const totalvalve = menuOptions.reduce((acc, curr) => acc + curr.price, 0)
+            list.innerHTML =
+
+             `
+            <li>
+                
+                <p>O valor total dos itens é: R$ ${totalvalve}</p>
+                
+            </li> 
+            `      }
+
+
 
 buttonshowAll.addEventListener('click', () => showAll(menuOptions))
 buttonMapAll.addEventListener('click', mapAllItems)
+sumAll.addEventListener('click', sumAllItems)
