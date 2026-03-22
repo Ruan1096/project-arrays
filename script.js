2605b1fc28ed1ffc269031af3a192fa6da9c6f2c
Ruan1096 <ruanreis1996@gmail.com>
2026-03-22 11:51:02 -0300

    alteraçaõ final map

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const list = document.querySelector('ul')
 const buttonshowAll = document.querySelector('.show-all')
 const buttonMapAll = document.querySelector('.map-all')
-let myLi = ''
 
-function showAll() {
-    menuOptions.forEach((product) => {
+
+function showAll( productsArray ) {
+
+    let myLi = ''
+
+    productsArray.forEach((product) => {
     myLi = myLi + `
             <li>
                 <img src="${product.src}" alt="${product.name}">
@@ -17,12 +20,17 @@ function showAll() {
 list.innerHTML = myLi
 }
 function mapAllItems() { 
+    console.log('chamei')
     const newPrice = menuOptions.map ((products) => ({
-        name: products.name,
-        price: products.price * 0.9,
-        src: products.src
+        ...products,
+
+         price: products.price * 0.9,
+
+       
     }))
+    showAll(newPrice)
+    console.log(newPrice)
 }   
 
-buttonshowAll.addEventListener('click', showAll)
+buttonshowAll.addEventListener('click', () => showAll(menuOptions))
 buttonMapAll.addEventListener('click', mapAllItems)
